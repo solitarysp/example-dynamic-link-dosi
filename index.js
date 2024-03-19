482e311aa07d7a5a4dc77e5edf170c40eee87c1f
Your Name <you@example.com>
2024-03-19 15:43:23 +0700

    a

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.get('*', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('dosi', (req, res) => {
+    const userAgent = req.headers['user-agent'];
+
+    if (userAgent.includes('Android')) {
+        res.redirect(301, 'https://play.google.com/store/apps/details?id=world.dosi');
+    } else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
+        res.redirect(301, 'itms-apps://apps.apple.com/us/app/dosi-digital-marketplace/id6473919354');
+    } else {
+        res.send('Hello World!');
+    }
+
+    res.end();
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });

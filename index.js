17853d5cc9e55cdb5e319e37233b5503ce096233
le.van.thanh <le.van.thanh@linecorp.com>
2024-04-08 16:30:49 +0700

    add

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const path = require('path');
-
+const url = require('url');
 const app = express();
 const port = 3000;
 
 // Phục vụ các tệp tĩnh từ thư mục 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/dosi', (req, res) => {
+app.get('/router', (req, res) => {
     const userAgent = req.headers['user-agent'];
-    console.log(JSON.stringify(req.headers))
 
-    if (userAgent.includes('Android')) {
-        res.redirect(301, 'https://play.google.com/store/apps/details?id=world.dosi');
-    } else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
-        res.redirect(301, 'itms-apps://apps.apple.com/us/app/dosi-digital-marketplace/id6473919354');
+    const queryObject = url.parse(req.url, true).query;
+
+    console.log(JSON.stringify(queryObject))
+
+    const openStorge = queryObject['openStorge'];
+    if (openStorge === 'true' || openStorge == '1') {
+        if (userAgent.includes('Android')) {
+            console.log("UserAgent type Android")
+            res.redirect(301, 'https://play.google.com/store/apps/details?id=world.dosi');
+        } else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
+            console.log("UserAgent type iPhone")
+            res.redirect(301, 'itms-apps://apps.apple.com/us/app/dosi-digital-marketplace/id6473919354');
+        } else {
+            console.log("Not fount userAgent type")
+            res.redirect(301, 'https://dosi.world/');
+        }
     } else {
-        res.send('Hello World!1');
+        const urlParam = queryObject['url'];
+        if (urlParam) {
+            res.redirect(301, urlParam);
+        }
     }
 
     res.end();
